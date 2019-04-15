b7d70a381f74d654b16aad5380485ca021d5e8e5
yuzumone <yuzumone@outlook.com>
2019-04-15 09:35:10 +0900

    Add yum package task

--- sync.sh
+++ sync.sh
@@ -14,6 +14,8 @@ Command:
     rm-dotfile              remove all dotfiles
     sync-apt                sync apt packages
     install-apt [package]   add apt package
+    sync-yum                sync yum packages
+    install-ymu [package]   add yum package
     help                    print this
 
 Options:
@@ -70,6 +72,26 @@ case ${1} in
         ;; esac
         echo "\e[32mInstall ${2} of apt\e[m"
         ansible-playbook -i localhost, -c local playbook.yml --tags install-apt-package --extra-vars="package=${2}" --ask-sudo-pass
+    ;;
+
+    sync-yum)
+        case ${2} in '--host') HOST=${3};; esac
+        if [ $HOST = localhost ]
+        then
+            ansible-playbook -i $HOST, -c local playbook.yml --tags sync-yum-packages --ask-sudo-pass
+        else
+            ansible-playbook -i $HOST, playbook.yml --tags sync-yum-packages --ask-sudo-pass
+        fi
+    ;;
+
+    install-yum)
+        case ${2} in "")
+            echo "\e[31mRequire package name...\e[m"
+            usage
+            exit 1
+        ;; esac
+        echo "\e[32mInstall ${2} of yum\e[m"
+        ansible-playbook -i localhost, -c local playbook.yml --tags install-yum-package --extra-vars="package=${2}" --ask-sudo-pass
     ;;
 
     help|--help|-h|"")
